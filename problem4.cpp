21208cce2d8636015bcec3b099e30ea73586df70
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-04 13:35:33 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
+int main() {
+    int n, prf = 0;  cin >> n;
     int a[n];
-    for(int i = 0; i<n; i++) {
-        cin >> a[i];
+    for(int i = 0; i<n; i++){ cin >> a[i];}
+    int mn = a[0];
+    for(int i = 0; i<n; i++){
+        prf = max(prf, a[i] - mn);
+        mn = min(mn, a[i]);
     }
-    int mnp = 2147483647, mxp = 0;
-    for(int i = 0; i < n; i++){
-        mnp = min(mnp, a[i]);
-        mxp = max(mxp, a[i]-mnp);
-    }
-    cout << mxp << endl;
+    cout << prf << endl;
     return 0;
 }
