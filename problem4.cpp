b6dd9e06eac1ba8b3fb42eef5ace014f00b5b566
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-04 13:24:07 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main(){
-    int n;  cin >> n;
-    if (n <= 1){    cout << "0" << endl;    return 0;}
+    int n;
+    cin >> n;
     int a[n];
-    for(int i = 0; i < n; i++){ cin >> a[i];}
-    int mnp = INT_MAX, mxp = 0;
-    for(int i=0; i<n; i++){
+    for(int i = 0; i<n; i++) {
+        cin >> a[i];
+    }
+    int mnp = 2147483647, mxp = 0;
+    for(int i = 0; i < n; i++){
         mnp = min(mnp, a[i]);
         mxp = max(mxp, a[i]-mnp);
     }
