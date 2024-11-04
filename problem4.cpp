912edffd1936ea3383a794e7ff8b83918c2682c3
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-11-04 13:10:53 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int n, r=0;  cin >> n;
+    int n;  cin >> n;
     int a[n];
     for(int i = 0; i < n; i++){ cin >> a[i];}
     int mnp = 2147483647, mxp = 0;
